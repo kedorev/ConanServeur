e35d0addd87afefc3f04715f2fbd6d7396d8c58b
kedorev <kedorev@gmail.com>
2021-07-01 20:49:07 +0200

    start server

---
+++ startServer.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export WINEARCH=win64
+export WINEPREFIX=/home/steam/.wine64
+
+screen -S coanan1
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine /home/steam/.steam/steamapps/common/Conan\ Exiles\ Dedicated\ Server/ConanSandboxServer.exe -log
