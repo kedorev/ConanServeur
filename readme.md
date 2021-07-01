37605554f540f2ecc4b02ce01619083a37fd28a8
kedorev <kedorev@gmail.com>
2021-07-01 21:04:05 +0200

    update start & readme

--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 #Command pour initialiser le projet
 
 sudo apt-get install git
+git clone https://github.com/kedorev/ConanServeur
+chmod +x init.sh
+chmod +x postSteamCMD.sh
+
+./init.sh
+
+Une fois la commande finie : sudo apt-get install steamcmd -y
+
+./postSteamCMD.sh
--- startServer.sh
+++ startServer.sh
@@ -2,5 +2,5 @@
 export WINEARCH=win64
 export WINEPREFIX=/home/steam/.wine64
 
-screen -S coanan1
+screen -S Conan1
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine /home/steam/.steam/steamapps/common/Conan\ Exiles\ Dedicated\ Server/ConanSandboxServer.exe -log
